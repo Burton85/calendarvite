a39b153f25fa116db92b6a87e833e365f5c52aa4
Burton <32318847+Burton85@users.noreply.github.com>
2021-04-12 17:52:11 +0800

    month page

--- src/views/WeekView.vue
+++ src/views/WeekView.vue
@@ -1,5 +1,5 @@
 <template>
-    <week-day :today="today" :tmpdate="tmpDate" @showMonth="showMonthHandlerParent()">></week-day>
+    <week-day :today="today" :tmpdate="tmpDate" @showMonth="showMonthHandlerParent()"></week-day>
     <ul class="week">
         <li class="week_item" v-for="(item, index) in 25 * 8" :key="item">
             <h2 class="week_item_title" v-if="index % 8 == 0">
@@ -7,14 +7,16 @@
             </h2>
         </li>
     </ul>
-    <month v-if="showMonth == true" @hideMonth="hideMonthHandlerParent()"></month>
+    <div class="calendarvite_btn_container">
+        <button class="calendarvite_btn" @click="showMonthHandlerParent()">+新增預約</button>
+    </div>
+    <month v-if="showMonth == true" @hideMonth="hideMonthHandlerParent()" :today="today" :toyear="toyear" :tomonth="tomonth"></month>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import WeekDay from '../components/WeekDay.vue';
 import Month from '../components/MonthView.vue';
 import useWeek from '../composition/useWeek';
-
 export default defineComponent({
     name: 'WeekView',
     components: {
